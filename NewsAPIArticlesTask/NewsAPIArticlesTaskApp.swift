983b53115981f39bb4d4da2e00a3b6637ff2f5df
Kanagasabapathy <trksabapathy7@gmail.com>
2024-02-01 14:27:06 +0530

    Initial Commit

---
+++ NewsAPIArticlesTask/NewsAPIArticlesTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIArticlesTaskApp.swift
+//  NewsAPIArticlesTask
+//
+//  Created by kanagasabapathy on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIArticlesTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
